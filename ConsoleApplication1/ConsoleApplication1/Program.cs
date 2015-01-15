404cd8232aab45a5f81182be4239e49f967994fb
Wyatt <wyatt.reid@gmail.com>
2015-01-14 20:05:40 -0500

    Initial Commit from Takeoff

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            string yearsOld = "";
+            string output = String.Format("{0} is {1} years old today!");
+
+            Console.WriteLine(hello);
+
+            Console.Read();
+        }
+    }
+
+    public class GetAge
+    {
+        string name = "Wyatt";
+        DateTime birthdate = DateTime.Parse("08/19/1991");
+
+        public string getName
+        {
+            get;
+        }
+    }
+}
