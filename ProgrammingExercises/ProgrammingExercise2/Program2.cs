4aabf2c448d3d37681a32e9ec9f1b05b01cc09d0
Wyatt <wyatt.reid@gmail.com>
2015-01-21 18:45:39 -0500

    Added some code to Exercise 2

--- ProgrammingExercises/ProgrammingExercise2/Program2.cs
+++ ProgrammingExercises/ProgrammingExercise2/Program2.cs
@@ -8,8 +8,19 @@ namespace ProgrammingExercise2
 {
     class Program
     {
+        static string OUTPUT_STATEMENT = "The Ohio State University has the best football team";
+
         static void Main(string[] args)
         {
+            
+            Console.Write("How many times to print the statement? (0-50): ");
+            var input = Console.ReadLine();
+
+            Console.Read();
+
+            
         }
     }
+
 }
+ 
