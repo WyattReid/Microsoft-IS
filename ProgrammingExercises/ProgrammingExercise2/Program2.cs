e232c47e19ea945cb3e573bff575ed2f119ecb4e
Wyatt <wyatt.reid@gmail.com>
2015-01-21 20:53:00 -0500

    Finish up

--- ProgrammingExercises/ProgrammingExercise2/Program2.cs
+++ ProgrammingExercises/ProgrammingExercise2/Program2.cs
@@ -4,36 +4,56 @@ namespace ProgrammingExercise2
 {
     class Program
     {
-        const string OutputStatement = "The Ohio State University has the best football team";
+        const string OutputStatement = "The Ohio State University has the best football team in the nation";
 
-        static void Main(string[] args)
+        private static void DoWrite(int times)
         {
-            var isInt = false;
-            Console.Write("How many times to print the statement? (0-50): ");
-            var input = Console.ReadLine();
-            int output;
-
-            isInt = int.TryParse(input, out output);
-
-            if (!isInt)
-            {
-                Console.WriteLine("ERROR: Please enter a number");
-            }
-
-            if (output > 50)
-            {
-                Console.WriteLine("ERROR: Please enter a number less than 50");
-            }
-
-            if (output == 0 || output < 0 )
-            {
-                Console.WriteLine("ERROR: Please enter a positive number");
-            }
-
-            for (var i=0; i < output; i++ )
+            for (var i = 0; i < times; i++)
             {
                 Console.WriteLine(OutputStatement);
             }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            var output = 0;
+            var flag = false;
+            while (!flag)
+            {
+                Console.Write("How many times to print the statement? (0-50): ");
+                var input = Console.ReadLine();
+                
+                var isInt = int.TryParse(input, out output);
+
+                if (!isInt)
+                {
+                    Console.WriteLine("ERROR: Please enter a number");
+                    
+
+                }
+
+                else if (output > 50)
+                {
+                    Console.WriteLine("ERROR: Please enter a number <= 50");
+                    
+                }
+
+                else if (output == 0 || output < 0)
+                {
+                    Console.WriteLine("ERROR: Please enter a positive number");
+
+                }
+                else
+                {
+                    flag = true;
+                    Console.WriteLine();
+                }
+
+
+            }
+            
+            DoWrite(output);
 
             Console.Read();
 
