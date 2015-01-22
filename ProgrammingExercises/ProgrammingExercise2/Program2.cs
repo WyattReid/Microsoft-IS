a0e5add5a6d9927b614d1dacdfb1c7cf9f959848
Wyatt <wyatt.reid@gmail.com>
2015-01-21 20:20:38 -0500

    Added some things

--- ProgrammingExercises/ProgrammingExercise2/Program2.cs
+++ ProgrammingExercises/ProgrammingExercise2/Program2.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgrammingExercise2
 {
     class Program
     {
-        static string OUTPUT_STATEMENT = "The Ohio State University has the best football team";
+        const string OutputStatement = "The Ohio State University has the best football team";
 
         static void Main(string[] args)
         {
-            
+            var isInt = false;
             Console.Write("How many times to print the statement? (0-50): ");
             var input = Console.ReadLine();
+            int output;
+
+            isInt = int.TryParse(input, out output);
+
+            if (!isInt)
+            {
+                Console.WriteLine("ERROR: Please enter a number");
+            }
+
+            if (output > 50)
+            {
+                Console.WriteLine("ERROR: Please enter a number less than 50");
+            }
+
+            if (output == 0 || output < 0 )
+            {
+                Console.WriteLine("ERROR: Please enter a positive number");
+            }
+
+            for (var i=0; i < output; i++ )
+            {
+                Console.WriteLine(OutputStatement);
+            }
 
             Console.Read();
 
